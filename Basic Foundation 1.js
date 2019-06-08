c59fd454d7fa62ebf5c3caed96a01a1499a0aa11
Lauren Wixom <50384167+laurenwixom@users.noreply.github.com>
2019-06-07 22:29:32 -0500

    Add files via upload

---
+++ Basic Foundation 1.js
@@ -0,0 +1,163 @@
+  for ( i=0;i<256;i++){
+ 	console.log (i);
+ }
+
+
+
+
+ for (var i=0; i <=1000; i++){
+ 	if (i<= 1000){
+ 		console.log (i++);
+
+ 	}
+
+ }
+
+
+
+
+x=1
+ for (vari=0;<=5000;i++){
+ 	if(i<=5000){
+ 		console.log (i+x);
+ 		return i+;
+ 	}
+ }
+
+
+
+
+function  a(b,c)
+{
+
+	console.log (b);
+	console.log (c);
+	}
+
+ a(4,3);
+
+
+
+
+var x= [1,2,3,4,5,6,7];
+console.log (x[6]);
+
+
+
+
+var x=[1,2,3];
+for ( var i = 0; i < x.length; i ++){
+    var avg = (x[i] / x.length) * x.length
+}
+
+
+
+
+
+
+
+function sum_odd(arr) {
+  for (var i = 0; i < 500; i += 2) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+var res = [];
+sum_odd(res)
+document.write(res);
+
+
+
+
+
+
+
+var x=[1,2,3,4]
+y=3{
+	(var i=0; i>3;x++){
+		if x[]>y;
+// struggling with this one
+
+	}
+	
+
+
+}
+
+
+
+
+
+
+
+function square(x) { return x * x; }
+console.log(square(4));
+
+
+
+
+
+
+
+
+var x=[1,2,3,-10]{
+	if x[3] >10;
+	x[3]= x[0]
+}
+{
+	else if x[3]<10;
+	x[3]=10
+}
+console.log x
+
+
+
+
+
+
+
+
+
+var num = [1,2,3,100];
+console.log(Math.max.apply(null, num)); 
+console.log(Math.min.apply (null,num));
+console.log var avg = sum/elmt.length;
+
+
+
+
+
+
+
+var x=[1,2,3,4,5]{
+[x[0],x[4]] = [x[4],x[0]];
+console.log x
+}
+
+
+
+
+
+
+
+
+
+
+
+var x=[-1,2,3];
+var counter = 0
+
+	if x[0]<=0;
+	{
+	console.log ('Dojo');
+
+}
+
+	else 
+{
+	console.log ('coding');
+}
+
+console.log x
+
